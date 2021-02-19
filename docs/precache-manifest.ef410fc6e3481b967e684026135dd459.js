2a6f4aa483771a8c67eb49aa884fde66194a1f11
asma496 <asmaashraf496@gmail.com>
2021-02-19 16:39:55 +0500

    ddone

---
+++ docs/precache-manifest.ef410fc6e3481b967e684026135dd459.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d017862edacbdb10db374bf14a56176",
+    "url": "index.html"
+  },
+  {
+    "revision": "74e24e9f3211001f2eb6",
+    "url": "static/css/main.64447240.chunk.css"
+  },
+  {
+    "revision": "e5a754383f15f4d6d10a",
+    "url": "static/js/2.0f345ad2.chunk.js"
+  },
+  {
+    "revision": "74e24e9f3211001f2eb6",
+    "url": "static/js/main.fda85c67.chunk.js"
+  },
+  {
+    "revision": "b1aeed9f6344163e5e40",
+    "url": "static/js/runtime~main.a3a648b2.js"
+  },
+  {
+    "revision": "54335fd02372f8eba1276f8a4c6a0ef7",
+    "url": "static/media/sad_dog.54335fd0.png"
+  }
+]);
